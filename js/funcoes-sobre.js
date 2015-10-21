8c7b94221dbf659ca23a1042c1ad1bf95e97f36d
Andre <dresch666@gmail.com>
2015-10-21 14:05:36 -0200

    Ajustada a velocidade da animacao na tela de sobre

--- js/funcoes-sobre.js
+++ js/funcoes-sobre.js
@@ -16,31 +16,31 @@ function tween(){
 		var e4 = $(".vitrine").offset().top;
 		var e5 = $("#educacional").offset().top;
 
-		if($(window).scrollTop() + ($(window).height() / 5 + 200) > e1 && !$('#toda-linha').hasClass('anim') ){
+		if($(window).scrollTop() + ($(window).height()) > e1 && !$('#toda-linha').hasClass('anim') ){
            $('#toda-linha').addClass('anim');
             TweenMax.fromTo($('.toda-linha h3'), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});
             TweenMax.fromTo($('.toda-linha hr'), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});
             TweenMax.fromTo($('.toda-linha .produto-destaque'), .93, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, delay: .10, ease: Quart.easeOut});
         }
 
-		if($(window).scrollTop() + ($(window).height() / 5 + 200) > e2 && !$('#mapa').hasClass('anim') ){
+		if($(window).scrollTop() + ($(window).height()) > e2 && !$('#mapa').hasClass('anim') ){
            $('#mapa').addClass('anim');
             TweenMax.fromTo($('.todo-brasil h3'), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});
             TweenMax.fromTo($('.todo-brasil p'), .93, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, delay: .10, ease: Quart.easeOut});
             TweenMax.fromTo($('.todo-brasil .btn'), .95, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, delay: .10, ease: Quart.easeOut});
         }
 
-        if($(window).scrollTop() + ($(window).height() / 5 + 200) > e3 && !$('#vendas-corporativas').hasClass('anim') ){
+        if($(window).scrollTop() + ($(window).height()) > e3 && !$('#vendas-corporativas').hasClass('anim') ){
            $('#vendas-corporativas').addClass('anim');
             TweenMax.fromTo($('.box-corporativo h4,.box-corporativo p'), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});
         }
 
-        if($(window).scrollTop() + ($(window).height() / 5 + 200) > e4 && !$('.vitrine').hasClass('anim') ){
+        if($(window).scrollTop() + ($(window).height()) > e4 && !$('.vitrine').hasClass('anim') ){
            $('.vitrine').addClass('anim');
             TweenMax.fromTo($('.vitrine ul'), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});
         }
        	
-       	if($(window).scrollTop() + ($(window).height() / 5 + 200) > e5 && !$('#educacional').hasClass('anim') ){
+       	if($(window).scrollTop() + ($(window).height()) > e5 && !$('#educacional').hasClass('anim') ){
            $('#educacional').addClass('anim');
             TweenMax.fromTo($('.box-educacional'), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});
         }
