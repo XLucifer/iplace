a9fe9549d58211dc031d1c0efab7a42c14812dd7
Andre <dresch666@gmail.com>
2015-10-28 15:23:37 -0200

    Adicionada a tela de cartoes

--- js/cartao.js
+++ js/cartao.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	barraAtivo();
-	//tween();
+	tween();
 
 	
 
@@ -22,17 +22,29 @@ $(document).ready(function(){
 
 function tween(){
 	$(window).on('scroll', function(){
-		var e1 = $(".box-consorcio").offset().top;
-		
+		var e1 = $(".box-anuidade").offset().top;
+		var e2 = $(".cartao").offset().top;
+		var e3 = $(".footer-grey").offset().top;
+		var e4 = $("#fatura").offset().top;
 		
 
-		if($(window).scrollTop() + ($(window).height() / 5 + 200) > e1 && !$('.box-consorcio').hasClass('anim') ){
-           $('.box-consorcio').addClass('anim');
-            TweenMax.fromTo($('.box-consorcio article'), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});
-            TweenMax.fromTo($('.intro .iphone'), .5, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});
+		if($(window).scrollTop() + ($(window).height()) > e1 && !$('.box-anuidade').hasClass('anim') ){
+           $('.box-anuidade').addClass('anim');
+            TweenMax.fromTo($("div",'.box-anuidade'), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});            
         }
-		
-        
+        if($(window).scrollTop() + ($(window).height()) > e2 && !$('.cartao').hasClass('anim') ){
+           $('.cartao').addClass('anim');
+            TweenMax.fromTo($("h3",'.cartao'), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});            
+            TweenMax.fromTo($("section",'.cartao'), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});
+        }
+		if($(window).scrollTop() + ($(window).height()) > e3 && !$('.footer-grey').hasClass('anim') ){
+           $('.footer-grey').addClass('anim');
+            TweenMax.fromTo($(".box-condicoes",'.footer-grey'), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});            
+        }
+        if($(window).scrollTop() + ($(window).height()) > e4 && !$('#fatura').hasClass('anim') ){
+           $('#fatura').addClass('anim');
+            TweenMax.fromTo($(".box-cartoes",'#fatura'), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});            
+        }
 	});
 }
 
