b165b8eb6dea1d7c0c5c38dc921439f58df99ef3
Andre <dresch666@gmail.com>
2015-10-31 23:48:29 -0200

    Criada a tela de BELKINS

---
+++ js/belkin.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+	//tween();
+
+});
+
+function tween(){
+	$(window).on('scroll', function(){
+		// var e1 = $("#cabecalho").offset().top;
+		// var e2 = $(".acessorios").offset().top;
+  //       var e3 = $(".banners").offset().top;
+  //       var e4 = $(".container-portable").offset().top;
+  //       var e5 = $(".produtos").offset().top;
+		
+
+        // if($(window).scrollTop() + ($(window).height()) > e1 && !$('#cabecalho').hasClass('anim') ){
+        //     $('#cabecalho').addClass('anim');
+        //       TweenMax.fromTo($('div:not(".desk")',"#cabecalho"), .9, {y: 20, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});
+        // }    
+	});
+}
