2f0fabef1c58ac0648cbe2013e61e37d43623029
Andre <dresch666@gmail.com>
2015-11-06 02:29:42 -0200

    Ajustada tela belkin para mobile, e criada atela Cazee

---
+++ js/cazee.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+	tween();
+
+	
+	
+	
+
+
+	// navegacao pelo menu
+	$(document).on('click', 'nav a', function() {
+		$(".menu-principal li").removeClass("ativo");
+		$(this).parent("li").addClass("ativo");
+		getnav = $(this).attr("href");
+		$("html,body").animate({
+			scrollTop: $(getnav).offset().top 
+		}, 900);	
+		
+		return false;
+	});	
+});
+
+function tween(){
+	$(window).on('scroll', function(){
+		var e1 = $(".box-beneficios").offset().top;
+		var e2 = $(".descubra").offset().top;
+		var e3 = $("#danos-quebras").offset().top;
+		var e4 = $(".article-beneficios").offset().top;
+		
+
+		if($(window).scrollTop() + ($(window).height()) > e1 && !$('.box-beneficios').hasClass('anim') ){
+           $('.box-beneficios').addClass('anim');
+            TweenMax.fromTo($('.topo-beneficios'), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});
+        }
+        if($(window).scrollTop() + ($(window).height()) > e2 && !$('.descubra').hasClass('anim') ){
+           $('.descubra').addClass('anim');
+            TweenMax.fromTo($('ul',".descubra"), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});
+        }
+        if($(window).scrollTop() + ($(window).height()) > e3 && !$('#danos-quebras').hasClass('anim') ){
+           $('#danos-quebras').addClass('anim');
+            TweenMax.fromTo($("img",'#danos-quebras div'), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});
+        }
+        if($(window).scrollTop() + ($(window).height()) > e4 && !$('.article-beneficios').hasClass('anim') ){
+           $('.article-beneficios').addClass('anim');
+            TweenMax.fromTo($('.article-beneficios div'), .9, {y: 80, z: 1, alpha: 0}, {y: 0, z: 0, alpha: 1, ease: Quart.easeOut});
+        }
+        
+	});
+}
